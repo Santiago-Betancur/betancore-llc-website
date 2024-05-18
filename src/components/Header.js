97deff7eb678392b36fd329bf530f95831a33e19
Santiago-Betancur <santibetac@gmail.com>
2024-05-18 04:11:10 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>BetanCore LLC</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
